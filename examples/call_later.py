c4c724bb2e4025eb5ef53e533a5711be87ad4218
Julian Berman <Julian@GrayVines.com>
2016-01-05 15:05:37 -0500

    Deterministic, and a runner.

--- examples/call_later.py
+++ examples/call_later.py
@@ -4,14 +4,19 @@ import transaction
 from inevitable.core import Reactor
 
 
-def callback():
-    for i in xrange(10000):
-        answer = i ** 3
-    if random.random() > 0.00001:
-        reactor.call_later(seconds=0, callback=callback)
+class Looper(object):
+    def __init__(self, iterations):
+        self.iterations = iterations
+
+    def run(self):
+        for i in xrange(10000):
+            answer = i ** 3
+        if self.iterations > 0:
+            self.iterations -= 1
+            reactor.call_later(seconds=0, callback=self.run)
 
 
 reactor = Reactor(clock=transaction)
-reactor.call_later(seconds=0, callback=callback)
-reactor.call_later(seconds=0, callback=callback)
+reactor.call_later(seconds=0, callback=Looper(iterations=1000).run)
+reactor.call_later(seconds=0, callback=Looper(iterations=1000).run)
 reactor.run_until_idle()
